5d376df3fb462ab886362051c3b3f58ecbd84011
admin <admin@YD05-19>
2024-05-31 17:50:01 +0900

    KSH0531

---
+++ boot02/src/main/java/com/yedam/app/test/web/TestController.java
@@ -0,0 +1,24 @@
+package com.yedam.app.test.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+	
+	//@RequestMapping(path="/test", method=RequestMethod.GET)
+	@GetMapping("test")
+	@ResponseBody
+	public String urlGetTest(String keyword) {
+		return "Server Response : SELECT - " + keyword;
+	}
+	
+	//@RequestMapping(path="/test", method=RequestMethod.POST)
+	@PostMapping("test")
+	@ResponseBody
+	public String urlPostTest(String keyword) {
+		return "Server Response : INSERT - " + keyword;
+	}
+}
